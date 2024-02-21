eb0cd68ecc85d2c6366cc9c2651d303ea6ae532e
miora rabearivelo <mioraharivelo261@gmail.com>
2024-02-21 15:15:28 +0300

    update models and models admin

--- common/admin.py
+++ common/admin.py
@@ -1,7 +1,8 @@
 
 
 from django.contrib import admin
-from .models import CustomerUser
+from .models import CustomerUser, Destination, Seats, Bagages,\
+    Driver, Car, Reservation, Validation, Payment
 
 class CustomerUserAdmin(admin.ModelAdmin):
     model = CustomerUser
@@ -13,3 +14,93 @@ class CustomerUserAdmin(admin.ModelAdmin):
     ]
 
 admin.site.register(CustomerUser, CustomerUserAdmin)
+
+
+class DestinationAdmin(admin.ModelAdmin):
+    model = Destination
+    ordering = ('id',)
+    search_fields = ('end_at',)
+    list_display = [
+        'id', 'start_at','end_at','costs'
+    ]
+
+admin.site.register(Destination, DestinationAdmin)
+
+
+class SeatsAdmin(admin.ModelAdmin):
+    model = Seats
+    ordering = ('seats_used',)
+    search_fields = ('seats_used',)
+    list_display = [
+        'seats_used', 'seats_total', 'seats_free'
+    ]
+
+admin.site.register(Seats, SeatsAdmin)
+
+
+class BagagesAdmin(admin.ModelAdmin):
+    model = Bagages
+    ordering = ('id',)
+    search_fields = ('customer',)
+    list_display = [
+        'id', 'customer', 'weigth', 'costs', 'bagage_name', 'descripion'
+    ]
+
+admin.site.register(Bagages, BagagesAdmin)
+
+
+class DriverAdmin(admin.ModelAdmin):
+    model = Driver
+    ordering = ('id',)
+    search_fields = ('first_name',)
+    list_display = [
+        'id', 'first_name', 'last_name','phone_number', 
+    ]
+
+admin.site.register(Driver, DriverAdmin)
+
+
+class CarAdmin(admin.ModelAdmin):
+    model = Car
+    ordering = ('car_number',)
+    search_fields = ('car_number',)
+    list_display = [
+        'car_number', 'driver', 'destination', 'seats', 'bagages', 'customer'
+    ]
+
+admin.site.register(Car, CarAdmin)
+
+class ReservationAdmin(admin.ModelAdmin):
+    model = Reservation
+    ordering = ('id',)
+    search_fields = ('customer',)
+    list_display = [
+        'id', 'customer', 'bagages', 'destination', 'customer', 
+        'payment', 'car', 'created_at', 'updated_at'
+    ]
+
+admin.site.register(Reservation, ReservationAdmin)
+
+
+class ValidationAdmin(admin.ModelAdmin):
+    model = Validation
+    ordering = ('id',)
+    search_fields = ('customer',)
+    list_display = [
+        'id', 'reservation','car', 'seats', 'bagages', 'customer',
+        'create_at', 'upated_at'
+    ]
+
+admin.site.register(Validation, ValidationAdmin)
+
+
+class PaymentAdmin(admin.ModelAdmin):
+    model = Payment
+    ordering = ('ref',)
+    search_fields = ('payment_mode',)
+    list_display = [
+        'ref', 'payment_mode'
+    ]
+
+admin.site.register(Payment, PaymentAdmin)
+
--- common/models.py
+++ common/models.py
@@ -2,7 +2,7 @@
 
 import uuid
 from django.db import models
-# from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser
 
 
 class Destination(models.Model):
@@ -10,6 +10,9 @@ class Destination(models.Model):
     start_at = models.CharField(max_length=50, default="")
     end_at = models.CharField(max_length=50,default="")
     costs = models.DecimalField(max_digits=15, decimal_places=2, default=0.0)
+    
+    def __str__(self):
+        return self.costs
 
 
 class Seats(models.Model):
@@ -17,11 +20,17 @@ class Seats(models.Model):
     seats_used = models.IntegerField(default=0)
     
     @property
-    def seats_free(self):
-        return self.seats_total - self.seat_free
+    def seats_free(self, *args, **kwargs):
+        if self.seats_used >= 1:
+            seats_disponible = self.seats_total - self.seats_used
+            return seats_disponible
+        super().save(*args, **kwargs)
     
-    def __str__(self):
-        return self.seats_total, self.seats_used, self.seats_free
+   
+
+
+"""class UserApp(AbstractUser):
+    pass"""
 
 
 class CustomerUser(models.Model):
@@ -29,7 +38,7 @@ class CustomerUser(models.Model):
     second_phone_number = models.IntegerField(default=0)
     customer_name = models.CharField(max_length=150)
     cin = models.IntegerField(default=0)
-    # seats = models.ManyToManyField(Seats)
+    seats = models.ManyToManyField(Seats)
     
     
     def save(self, *args, **kwargs):
@@ -63,6 +72,9 @@ class Bagages(models.Model):
             self.costs = self.weigth * 500 
         return self.costs 
     
+    def __str__(self):
+        return self.customer
+    
 
             
 class Driver(models.Model):
@@ -72,14 +84,54 @@ class Driver(models.Model):
     phone_number = models.IntegerField(default=0)
     image = models.ImageField(upload_to='images/')
     
+    def __str__(self):
+        return self.first_name
+    
 
 class Car(models.Model):
     car_number = models.CharField(primary_key=True, editable=True, unique=True, max_length=30)
     destination = models.ForeignKey(Destination, on_delete=models.SET_NULL, null=True)
-    driver = models.ManyToManyField(Driver)
-    seats = models.ManyToManyField(Seats)
-    bagages = models.ManyToManyField(Bagages)
-    customer = models.ManyToManyField(CustomerUser)
+    driver = models.ForeignKey(Driver, on_delete=models.SET_NULL, null=True)
+    seats = models.ForeignKey(Seats, on_delete=models.SET_NULL, null=True)
+    bagages = models.ForeignKey(Bagages, on_delete=models.SET_NULL, null=True)
+    customer = models.ForeignKey(CustomerUser, on_delete=models.SET_NULL, null=True)
+    
+    def __str__(self):
+        return self.car_number
+    
+
+class Payment(models.Model):
+    ref = models.CharField(primary_key=True, editable=False, max_length=100)
+    payment_mode = models.CharField(max_length=30, default="")
+    
+    def __str__(self):
+        return self.ref
+
+
+class Reservation(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    customer = models.ForeignKey(CustomerUser, on_delete=models.SET_NULL, null=True)
+    bagages = models.ForeignKey(Bagages, on_delete=models.SET_NULL, null=True)
+    destination = models.ForeignKey(Destination, on_delete=models.SET_NULL, null=True)
+    payment = models.ForeignKey(Payment, on_delete=models.SET_NULL, null=True)
+    car = models.ForeignKey(Car, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)    
+
+    def __str__(self):
+        return self.customer
     
     
+class Validation(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    # user_validator = models.ForeignKey(UserApp, on_delete=models.SET_NULL, null=True)
+    reservation = models.ForeignKey(Reservation, on_delete=models.SET_NULL, null=True)
+    customer = models.ForeignKey(CustomerUser, on_delete=models.SET_NULL, null=True)
+    bagages = models.ForeignKey(Bagages, on_delete=models.SET_NULL, null=True)
+    seats = models.ForeignKey(Seats, on_delete=models.SET_NULL, null=True)
+    car = models.ForeignKey(Car, on_delete=models.SET_NULL, null=True)
+    create_at = models.DateTimeField(auto_now_add=True)
+    upated_at = models.DateTimeField(auto_now=True)
     
+    def __str__(self):
+        return self.customer
