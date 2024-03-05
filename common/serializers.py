11f1dd327d236180f0e93345eb90c66167af1667
RABEARIVELO Nandrasana Miora <miora.rabearivelo@pulse.mg>
2024-03-05 14:48:54 +0300

    update class DestinationSerializer

--- common/serializers.py
+++ common/serializers.py
@@ -5,6 +5,9 @@ from rest_framework import serializers
 from .models import Destination
 
 class DestinationSerializer(serializers.ModelSerializer):
+    start_at = serializers.DateTimeField(format="%d/%b/%Y")
+    end_at  = serializers.DateTimeField(format="%d/%b/%Y")
+    costs = serializers.DecimalField(max_digits=15, decimal_places=2)
     class Meta:
         model = Destination
         fields = ['id', 'start_at', 'end_at', 'costs']
