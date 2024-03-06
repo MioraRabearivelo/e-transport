66f4ed2ddd96d0890793a274036fff47173e340d
RABEARIVELO Nandrasana Miora <miora.rabearivelo@pulse.mg>
2024-03-06 16:00:09 +0300

    update urls

--- common/urls.py
+++ common/urls.py
@@ -5,5 +5,9 @@ from django.urls import path
 from .views import DestinationMixin
 
 urlpatterns = [
-    path('destination/', DestinationMixin.as_view(), name='destination'),
+    path('destiantion/<str:pk>/detail',   DestinationMixin.as_view()),
+    path('destiantion/<str:pk>/update',   DestinationMixin.as_view()),
+    path('destiantion/<str:pk>/delete',   DestinationMixin.as_view()),
+    path('destiantion/list/',   DestinationMixin.as_view()),
+    path('destiantion/create/',   DestinationMixin.as_view()),
 ]
