886913a579592f986b625e78a65f7b981efa1cae
RABEARIVELO Nandrasana Miora <miora.rabearivelo@pulse.mg>
2024-03-06 15:19:17 +0300

    fix destination url

--- common/urls.py
+++ common/urls.py
@@ -5,5 +5,5 @@ from django.urls import path
 from .views import DestinationMixin
 
 urlpatterns = [
-    path('destinantion/', DestinationMixin.as_view(), name='destination'),
+    path('destination/', DestinationMixin.as_view(), name='destination'),
 ]
