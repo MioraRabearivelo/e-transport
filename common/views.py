60c698dfe6d6820ab93a2150b7d8097936e283ec
RABEARIVELO Nandrasana Miora <miora.rabearivelo@pulse.mg>
2024-03-06 14:56:49 +0300

    create destiantion view

--- common/views.py
+++ common/views.py
@@ -1,4 +1,43 @@
-from django.shortcuts import render
 
 
-# Create your views here.
+from rest_framework import generics, mixins
+
+from .models import Destination
+from .serializer import DestinationSerializer
+
+
+class DestinationMixin(generics.GenericAPIView, 
+        mixins.CreateModelMixin, 
+        mixins.ListModelMixin, 
+        mixins.UpdateModelMixin, 
+        mixins.DestroyModelMixin, 
+        mixins.RetrieveModelMixin):
+    
+    queryset = Destination.objects.all()
+    serializer_class = DestinationSerializer
+    
+    def perform_create(self, serializer):
+        start_date = serializer.validated_data.get('start_date')
+        end_date =  serializer.validated_data.get('end_date')
+        costs =  serializer.validated_data.get('costs')
+        if start_date is None:
+            raise ValueError("Start date fields is required")
+        elif end_date is None:
+            raise ValueError("End date fields is required")
+        elif costs is None:
+            raise ValueError("Cost fields is required")
+        else:
+            serializer.save()
+            
+    def perform_update(self, serializer):
+        start_date = serializer.validated_data.get('start_date')
+        end_date =  serializer.validated_data.get('end_date')
+        costs =  serializer.validated_data.get('costs')
+        if start_date is None:
+            raise ValueError("Start date fields is required")
+        elif end_date is None:
+            raise ValueError("End date fields is required")
+        elif costs is None:
+            raise ValueError("Cost fields is required")
+        else:
+            serializer.save()
