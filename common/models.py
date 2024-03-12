278284368a5dd8c7ac05a2bcbf329918ed646d6f
RABEARIVELO Nandrasana Miora <miora.rabearivelo@pulse.mg>
2024-03-12 10:42:02 +0300

    add class registration

--- common/models.py
+++ common/models.py
@@ -2,45 +2,9 @@
 
 import uuid
 from django.core.exceptions import ValidationError
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
+from django.contrib.auth.models import User
 
-
-class CustomUserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
-        if not email:
-            raise ValueError('The Email field must be set')
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-
-        return self.create_user(email, password, **extra_fields)
-    
-
-class CustomUser(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(unique=True)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=50)
-    pseudo = models.CharField(primary_key=True, editable=True, unique=True, max_length=50)
-    phone_number = models.IntegerField(default=0)
-    is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
-    image = models.ImageField(upload_to='images/user/')
-    
-    objects = CustomUserManager()
-    
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["pseudo", "first_name", "last_name", "image", "phone_number"]
-    
-    def __str__(self) :
-        return self.pseudo
-    
 
 class Destination(models.Model):
     id = models.CharField(primary_key=True, max_length=10, unique=True, editable=False)
@@ -152,7 +116,7 @@ class Reservation(models.Model):
     
 class Registration(models.Model):
     id = models.IntegerField(primary_key=True, editable=False, unique=True)
-    user_validator = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    user_validator = models.ForeignKey(User, on_delete=models.CASCADE)
     reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE)
     bagages = models.ForeignKey(Bagages, on_delete=models.CASCADE)
     car = models.ForeignKey(Car, on_delete=models.CASCADE)
