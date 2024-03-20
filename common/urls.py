a4d00d8fc3a4dc9ecc667eb30b140bb75c8f03d2
RABEARIVELO Nandrasana Miora <miora.rabearivelo@pulse.mg>
2024-03-20 14:30:25 +0300

    update urls

--- common/urls.py
+++ common/urls.py
@@ -2,7 +2,7 @@
 
 from django.urls import path
 
-from .views import DestinationApiMixin, ListDriver
+from .views import DestinationApiMixin, ListDriver, CreateDriver, UpdateDriver, DeleteDriver
 
 urlpatterns = [
     path('destiantion/<str:pk>/detail',   DestinationApiMixin.as_view()),
@@ -10,6 +10,9 @@ urlpatterns = [
     path('destiantion/<str:pk>/delete',   DestinationApiMixin.as_view()),
     path('destiantion/list/',   DestinationApiMixin.as_view()),
     path('destiantion/create/',   DestinationApiMixin.as_view()),
-    path('driver/list_drivers/', ListDriver.as_view(), name='list-driver'),
-    path('driver/detail_drivers/<str:pk>/', ListDriver.as_view(), name='detail-driver')
+    path('driver/list-drivers/', ListDriver.as_view()),
+    path('driver/detail-driver/<str:pk>/', ListDriver.as_view(), name='detail-driver'),
+    path('driver/create/', CreateDriver.as_view()),
+    path('driver/update/<str:pk>/', UpdateDriver.as_view()),
+    path('driver/delete/<str:pk>/', DeleteDriver.as_view()),
 ]
